81e197c5fa756a5d3c9b71b3115e88d383e1af7c
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2013-07-15 12:53:52 +0200

    bugfix

--- LwAuthorityKeeper.php
+++ LwAuthorityKeeper.php
@@ -19,11 +19,10 @@
 *  
 ***************************************************************************/
 
-AuthorityNameSpace LwAuthorityKeeper;
+namespace LwAuthorityKeeper;
 
 class LwAuthorityKeeper
 {
-
     private $AuthorityNameSpace;
     private $loggedIn;
     private $SessionData;
